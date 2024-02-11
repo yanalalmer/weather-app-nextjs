dbf843e2fa7e184e269ad1ec0bdc1b39904d0b7d
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2024-02-11 13:18:38 +0100

    added weather card component

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -6,7 +6,7 @@ const API = axios.create({
   baseURL,
 });
 
-export const fetchWeather = (cityName = 'amsterdam') => {
+export const fetchWeather = (cityName = 'london') => {
   const requestUrl = `${baseURL}?q=${cityName}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
   return API.get(requestUrl).then(({ data }) => data);
--- src/components/layout/weather-card/WeatherCard.jsx
+++ src/components/layout/weather-card/WeatherCard.jsx
@@ -3,9 +3,10 @@ import { fetchWeather } from '@/api';
 import { useFetchWeather } from '@/hooks';
 
 export const WeatherCard = () => {
+  const cityName = 'amsterdam';
   const { weather, loading, error, errorMessage } = useFetchWeather(
     fetchWeather,
-    'london'
+    cityName
   );
   console.log(weather);
   return <div>weather card</div>;
