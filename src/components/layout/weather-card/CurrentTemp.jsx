beef066f776915a06dce94d09994e0af5fc4eae3
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2024-02-12 19:27:19 +0100

    added weather icons

--- src/components/layout/weather-card/CurrentTemp.jsx
+++ src/components/layout/weather-card/CurrentTemp.jsx
@@ -10,10 +10,6 @@ export const CurrentTemp = () => {
   return (
     <section className='flex items-center justify-center desktop:justify-start desktop:w-1/2 w-fit desktop:border-r-4 border-b-4 desktop:border-b-0 mb-12 desktop:mb-0'>
       <WeatherIcon iconname={cityWeather?.weather[0]?.icon} className='w-48' />
-      {/* <img
-        src={`https://openweathermap.org/img/wn/${cityWeather?.weather[0]?.icon}@4x.png`}
-        alt='weather icon'
-      /> */}
       <div>
         <h1 className='dekstop:text-6xl text-4xl font-semibold mb-1'>
           {Math.round(cityWeather?.main?.temp)}°
--- src/components/layout/weather-card/CurrentWeatherInfo.jsx
+++ src/components/layout/weather-card/CurrentWeatherInfo.jsx
@@ -1,7 +1,14 @@
 'use client';
 import { useRecoilValue } from 'recoil';
 import { mainState } from '@/state';
-import { formatUnixTimeStamp } from '@/lib';
+import {
+  HumidityIcon,
+  LowTempIcon,
+  HighTempIcon,
+  VisibilityIcon,
+  WindIcon,
+  PressureIcon,
+} from '@/public/icons';
 
 export const CurrentWeatherInfo = () => {
   const weather = useRecoilValue(mainState);
@@ -11,32 +18,46 @@ export const CurrentWeatherInfo = () => {
     {
       value: Math.round(currentWeatherInfo?.main?.temp_max) + '°',
       label: 'High',
+      icon: <HighTempIcon />,
     },
     {
       value: Math.round(currentWeatherInfo?.wind?.speed) + ' ' + 'Mph',
       label: 'Wind',
+      icon: <WindIcon />,
     },
     {
       value: currentWeatherInfo?.main?.pressure + ' ' + 'hPa',
       label: 'pressure',
+      icon: <PressureIcon />,
     },
     {
       value: Math.round(currentWeatherInfo?.main?.temp_min) + '°',
       label: 'Low',
+      icon: <LowTempIcon />,
     },
-    { value: currentWeatherInfo?.main?.humidity + '%', label: 'humidity' },
+    {
+      value: currentWeatherInfo?.main?.humidity + '%',
+      label: 'humidity',
+      icon: <HumidityIcon />,
+    },
     {
       value: Math.round(currentWeatherInfo?.visibility / 1000) + ' ' + 'km',
       label: 'visibility',
+      icon: <VisibilityIcon />,
     },
   ];
 
   return (
     <section className='desktop:w-1/2 w-full grid grid-cols-3 grid-rows-2 gap-4 h-full'>
       {data.map((item, index) => (
-        <div className='flex justify-center items-center flex-col' key={index}>
+        <div className='text-center' key={index}>
           <div className='font-bold'>{item?.value}</div>
-          <div className='capitalize'>{item?.label}</div>
+          <div className='capitalize flex justify-center items-center flex-col'>
+            <div className='w-6 fill-black dark:fill-white stroke-black dark:stroke-white main-transition'>
+              {item?.icon}
+            </div>
+            <div>{item?.label}</div>
+          </div>
         </div>
       ))}
     </section>
