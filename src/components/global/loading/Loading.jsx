55aa962e841be1bed6aed8f6cf82709f6d027a6e
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2024-02-13 22:17:18 +0100

    new loading component

--- src/components/global/loading/Loading.jsx
+++ src/components/global/loading/Loading.jsx
@@ -1,7 +1,12 @@
 export const Loading = () => {
   return (
-    <div className='flex items-center h-screen w-full justify-center z-10 fixed top-0 left-0 bg-white dark:bg-black text-black dark:text-white'>
-      <p className='animate-bounce'>Loading....</p>
+    <div class='flex justify-center items-center flex-col bg-white h-screen w-full dark:bg-black z-10 fixed top-0 left-0'>
+      <div className='flex space-x-2 justify-center items-center mb-4'>
+        <div class='h-8 w-8 bg-black dark:bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+        <div class='h-8 w-8 bg-black dark:bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+        <div class='h-8 w-8 bg-black dark:bg-white rounded-full animate-bounce'></div>
+      </div>
+      <h1 class='block text-4xl font-bold'>Loading...</h1>
     </div>
   );
 };
